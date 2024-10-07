018000c1a560307e381ad0b1c3ab36dd216f5b36
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2024-10-08 03:46:59 +0800

    fix(RecipeGenerator): Remove `cookingTime`

--- src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/datagen/RecipeGenerator.kt
+++ src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/datagen/RecipeGenerator.kt
@@ -17,65 +17,35 @@ import java.util.concurrent.CompletableFuture
 class RecipeGenerator(output: FabricDataOutput, registriesFuture: CompletableFuture<RegistryWrapper.WrapperLookup>) :
     FabricRecipeProvider(output, registriesFuture) {
     override fun generate(exporter: RecipeExporter) {
-        campfireRecipe(exporter, MCItems.ROTTEN_FLESH, Items.COOKED_ROTTEN_FLESH, 0.15f, 600)
-        smokingRecipe(exporter, MCItems.ROTTEN_FLESH, Items.COOKED_ROTTEN_FLESH, 0.15f, 100)
-        smeltingRecipe(exporter, MCItems.ROTTEN_FLESH, Items.COOKED_ROTTEN_FLESH, 0.15f, 200)
+        campfireRecipe(exporter, MCItems.ROTTEN_FLESH, Items.COOKED_ROTTEN_FLESH, 0.15f)
+        smokingRecipe(exporter, MCItems.ROTTEN_FLESH, Items.COOKED_ROTTEN_FLESH, 0.15f)
+        smeltingRecipe(exporter, MCItems.ROTTEN_FLESH, Items.COOKED_ROTTEN_FLESH, 0.15f)
 
-        campfireRecipe(exporter, MCItems.CARROT, Items.COOKED_CARROT, 0.35f, 600)
-        smokingRecipe(exporter, MCItems.CARROT, Items.COOKED_CARROT, 0.35f, 100)
-        smeltingRecipe(exporter, MCItems.CARROT, Items.COOKED_CARROT, 0.35f, 200)
+        campfireRecipe(exporter, MCItems.CARROT, Items.COOKED_CARROT, 0.35f)
+        smokingRecipe(exporter, MCItems.CARROT, Items.COOKED_CARROT, 0.35f)
+        smeltingRecipe(exporter, MCItems.CARROT, Items.COOKED_CARROT, 0.35f)
     }
 
-    private fun campfireRecipe(
-        exporter: RecipeExporter,
-        input: Item,
-        output: Item,
-        experience: Float,
-        cookingTime: Int
-    ) {
+    private fun campfireRecipe(exporter: RecipeExporter, input: Item, output: Item, experience: Float) {
         CookingRecipeJsonBuilder.createCampfireCooking(
             Ingredient.ofItems(input),
             RecipeCategory.FOOD,
             output,
             experience,
-            cookingTime
+            600
         )
             .criterion(hasItem(input), conditionsFromItem(input))
             .offerTo(exporter, Identifier.of(Utils.MOD_ID, "${getRecipeName(output)}_from_campfire_cooking"))
     }
 
-    private fun smokingRecipe(
-        exporter: RecipeExporter,
-        input: Item,
-        output: Item,
-        experience: Float,
-        cookingTime: Int
-    ) {
-        CookingRecipeJsonBuilder.createSmoking(
-            Ingredient.ofItems(input),
-            RecipeCategory.FOOD,
-            output,
-            experience,
-            cookingTime
-        )
+    private fun smokingRecipe(exporter: RecipeExporter, input: Item, output: Item, experience: Float) {
+        CookingRecipeJsonBuilder.createSmoking(Ingredient.ofItems(input), RecipeCategory.FOOD, output, experience, 100)
             .criterion(hasItem(input), conditionsFromItem(input))
             .offerTo(exporter, Identifier.of(Utils.MOD_ID, "${getRecipeName(output)}_from_smoking"))
     }
 
-    private fun smeltingRecipe(
-        exporter: RecipeExporter,
-        input: Item,
-        output: Item,
-        experience: Float,
-        cookingTime: Int
-    ) {
-        CookingRecipeJsonBuilder.createSmelting(
-            Ingredient.ofItems(input),
-            RecipeCategory.FOOD,
-            output,
-            experience,
-            cookingTime
-        )
+    private fun smeltingRecipe(exporter: RecipeExporter, input: Item, output: Item, experience: Float) {
+        CookingRecipeJsonBuilder.createSmelting(Ingredient.ofItems(input), RecipeCategory.FOOD, output, experience, 200)
             .criterion(hasItem(input), conditionsFromItem(input))
             .offerTo(exporter, Identifier.of(Utils.MOD_ID, getRecipeName(output)))
     }
