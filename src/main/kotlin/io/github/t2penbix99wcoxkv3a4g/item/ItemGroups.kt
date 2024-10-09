91cb4b97e80c6b6257fd2d3626b2b61029a574cd
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2024-10-09 18:44:41 +0800

    fix(ItemGroups): Add `MOD_GROUP_KEY`

--- src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/item/ItemGroups.kt
+++ src/main/kotlin/io/github/t2penbix99wcoxkv3a4g/item/ItemGroups.kt
@@ -4,12 +4,17 @@ import eu.pb4.polymer.core.api.item.PolymerItemGroupUtils
 import io.github.t2penbix99wcoxkv3a4g.Utils
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
+import net.minecraft.registry.Registries
+import net.minecraft.registry.RegistryKey
 import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 
 object ItemGroups {
     @JvmStatic
-    var MOD_GROUP: ItemGroup = PolymerItemGroupUtils.builder()
+    var MOD_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(Utils.MOD_ID, "item-group"))
+    
+    @JvmStatic
+    var MOD_GROUP = PolymerItemGroupUtils.builder()
         .displayName(Text.translatable("item-group.${Utils.MOD_ID}.item-group"))
         .icon { ItemStack(Items.COOKED_ROTTEN_FLESH) }
         .entries { _: ItemGroup.DisplayContext?, entries: ItemGroup.Entries ->
@@ -20,7 +25,7 @@ object ItemGroups {
 
     @JvmStatic
     fun register() {
-        PolymerItemGroupUtils.registerPolymerItemGroup(Identifier.of(Utils.MOD_ID, "item-group"), MOD_GROUP)
+        PolymerItemGroupUtils.registerPolymerItemGroup(MOD_GROUP_KEY, MOD_GROUP)
         Utils.logger.info("Register Items")
     }
 }
